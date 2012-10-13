7fb18841685bdc0dd75594926fbbba8994025226
Ekta Mehta <mehta.ekta@gmail.com>
2012-10-13 12:19:16 -0400

    authentication working with google

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	google: {
+		clientId: '863563476508.apps.googleusercontent.com'
+      , clientSecret: '6k9HI9LeB6KEOq0cnCqj5EvW'
+    }
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var everyAuth = require('everyauth'),
 	connect = require('connect'),
 	express = require('express'),
-	Promise = everyAuth.Promise;
+	Promise = everyAuth.Promise,
+	config = require('./config')
 
 everyAuth.debug = true;
 
@@ -20,7 +21,23 @@ everyAuth.googlehybrid
 			// usersByGoogleHybridId[userAttributes.claimedIdentifier] || (usersByGoogleHybridId[userAttributes.claimedIdentifier] = addUser('googlehybrid', userAttributes))
 
 	})
-	.redirectPath('/')
+	.redirectPath('/home')
+
+everyAuth.google
+	.myHostname('http://www.everyauthdemo.com:5000')
+	.appId(config.google.clientId)
+	.appSecret(config.google.clientSecret)
+	.scope('https://www.googleapis.com/auth/userinfo.profile https://www.google.com/m8/feeds/') // What you want access to
+	.findOrCreateUser( function (sess, accessToken, extra, googleUser) {
+		console.log('##################', sess);
+		var promise = this.Promise(); 
+        promise.fulfill('test'); 
+        return promise; 
+		// googleUser.refreshToken = extra.refresh_token;
+		// googleUser.expiresIn = extra.expires_in;
+		// return usersByGoogleId[googleUser.id] || (usersByGoogleId[googleUser.id] = addUser('google', googleUser));
+	})
+	.redirectPath('/home');
 
 app = module.exports = express.createServer();
 app.configure(function(){
@@ -32,24 +49,29 @@ app.configure(function(){
 	app.set('view engine', 'html')
 	app.set('view options', { layout: false, pretty: true })
 
-		
-
 	// # Middleware
 	app.use(express.static(__dirname + '/public'))
 	app.use(express.bodyParser())
-	// app.use(express.router())
+	app.use(express.cookieParser())
+	app.use(express.session({ secret: 'everyauthdemo'})) 
+	//htuayreve evernodedemo
 	app.use(everyAuth.middleware())
 	
 });
 
-app.get('/auth/googlehybrid/callback', function(req, res) {
+app.get('/auth/google/callback', function(req, res) {
 	console.log('user authenticated');
 });
 
 app.get('/', function (req, res) {
-  res.render('login');
+	res.render('login');
+});
+
+app.get('/home', function (req, res) {
+	res.render('home');
 });
 
 port = process.env.PORT || 5000
 app.listen(port)
 console.log("Express server listening on port %d in %s mode", port, app.settings.env)
+
